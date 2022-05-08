7cd2329d6358fd5c5ed3c4c7c7ffd56107c83a3e
AlexeyDe <103239096+AlexeyDe@users.noreply.github.com>
2022-05-08 17:52:34 +0300

    HomeWork 12 module

--- Homework12.py
+++ Homework12.py
@@ -4,8 +4,6 @@ per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 deposit = list(per_cent.values())
 for i, num in enumerate(deposit): deposit[i] = deposit[i] * (money)/100
 for i, num in enumerate(deposit): deposit[i] = round(deposit[i])
+print('deposit', '=', deposit)
 deposit.sort()
 print('Максимальная сумма, которую вы можете заработать ', '- ', deposit[-1], ' руб.')
-
-
-
